63c624db5432297bd599f2b97a0c498950674058
jaylin <jaylin6115@gmail.com>
2023-02-01 18:44:05 +0900

    [FEAT] Login: MemberDetailsImpl, MemberDetailsServiceImpl, SignupRequestDto 추가 [해결]

--- src/main/java/com/example/artizen/security/MemberDetailsServiceImpl.java
+++ src/main/java/com/example/artizen/security/MemberDetailsServiceImpl.java
@@ -1,4 +1,20 @@
 package com.example.artizen.security;
 
+import com.example.artizen.entity.Member;
+import com.example.artizen.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberDetailsServiceImpl {
+
+    private final MemberRepository memberRepository;
+
+    public MemberDetailsImpl loadMemberByMembername(String nickname) {
+        Member member = memberRepository.findByNickname(nickname).orElseThrow(
+                () -> new IllegalArgumentException("Can't find" + nickname));
+
+        return new MemberDetailsImpl(member);
+    }
 }
