c2cbc8b8fc2244102d7ea20367023239aab74c6d
jaylin <jaylin6115@gmail.com>
2023-03-08 22:35:20 +0900

    [FEAT] Login admin 계정 인증 기능 추가 [해결]

--- src/main/java/com/example/artizen/controller/MemberController.java
+++ src/main/java/com/example/artizen/controller/MemberController.java
@@ -1,31 +1,47 @@
 package com.example.artizen.controller;
 
+import com.example.artizen.security.MemberDetailsImpl;
 import com.example.artizen.service.MemberService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/members")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping(value = "/user/kakao/callback")
-    public String  kakaoCallback() {
+    @GetMapping(value = "/kakao/callback")
+    public String kakaoCallback() {
         return "로그인이 진행되는 중입니다.";
     }
 
-    @GetMapping(value = "/members/kakaoLogin")
+    @GetMapping(value = "/kakaoLogin")
     public ResponseEntity<?> kakaoToken(@RequestParam String accessToken) throws JsonProcessingException {
         return memberService.kakaoLogin(accessToken);
     }
 
-    @GetMapping(value = "/members/logout")
+    @GetMapping(value = "/logout")
     public ResponseEntity<?> kakaoLogout(@RequestParam String accessToken) {
         return memberService.kakaoLogout(accessToken);
+    }
+
+//    @PostMapping(value = "/signup")
+//    public ResponseEntity<?> signup(@RequestBody SignupRequestDto signupRequestDto) {
+//        return memberService.signup(signupRequestDto);
+//    }
+//
+//    @GetMapping(value = "/login")
+//    public ResponseEntity<?> login() {
+//        return memberService
+//    }
+
+    @GetMapping(value = "/admin")
+    public ResponseEntity<?> getAdmin(@AuthenticationPrincipal MemberDetailsImpl memberDetails, @RequestParam String adminToken) {
+        return memberService.getAdmin(memberDetails.getMember().getId(), adminToken);
     }
 }
